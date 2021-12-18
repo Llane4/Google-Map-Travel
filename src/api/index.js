aba4e538c293777b19a66013c3a7d6b27bbd4d22
Leonel Llane <llane4@hotmail.es>
2021-12-18 03:31:13 -0300

    Funciona el api del mapa

---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        "x-rapidapi-key": "bdf713f50bmshe3c2e3b4b6ba3e0p19aa09jsnd4d4e136fb19",
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -8,13 +8,22 @@ import {
   FormControl,
   Select,
 } from "@material-ui/core";
-
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
 import useStyles from "./styles";
 
 const List = () => {
   const classes = useStyles();
   const [type, setType] = useState("restaurants");
   const [rating, setRating] = useState("restaurants");
+
+  const places = [
+    { name: "Buen Lugar" },
+    { name: "Mejor cerveza" },
+    { name: "Mejor comida" },
+    { name: "Buen Lugar" },
+    { name: "Mejor cerveza" },
+    { name: "Mejor comida" },
+  ];
 
   return (
     <div className={classes.container}>
@@ -36,6 +45,13 @@ const List = () => {
           <MenuItem value={4.5}>Above 4.5</MenuItem>
         </Select>
       </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid item key={i} xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
